0a1b31b124eada671cca5b35bb51572eb4b7cf4a
wfuller <wfuller87@gmail.com>
2019-07-09 09:36:48 -0700

    Adding footer and new gray scale stop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 
 import Navbar from './Navbar';
 import Routes from '../views/Routes';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
       <Routes />
+      <Footer />
     </Fragment>
   );
 };
