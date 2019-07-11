ee4076b485a1137139cc6480a6fbc8c81893af3e
wfuller <wfuller87@gmail.com>
2019-07-11 00:19:38 -0700

    Adding Google Analytics
    Google Analytics HOC

--- src/index.jsx
+++ src/index.jsx
@@ -7,17 +7,20 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 
 import theme from './theme';
 import App from './components/App';
+import GoogleAnalytics from './components/GoogleAnalytics';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
     <BrowserRouter>
-      <ParallaxProvider>
-        <Fragment>
-          <CssBaseline />
-          <App />
-        </Fragment>
-      </ParallaxProvider>
+      <GoogleAnalytics trackingId="UA-33461307-5">
+        <ParallaxProvider>
+          <Fragment>
+            <CssBaseline />
+            <App />
+          </Fragment>
+        </ParallaxProvider>
+      </GoogleAnalytics>
     </BrowserRouter>
   </MuiThemeProvider>,
   document.getElementById('root')
