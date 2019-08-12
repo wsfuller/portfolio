eb3175456ffffd9a824cbd7c751ef2641e366507
wfuller <wfuller87@gmail.com>
2019-08-11 22:02:39 -0700

    Adding react-snap
    Prerendering pages for better SEO support

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -10,21 +10,26 @@ import App from './components/App';
 import GoogleAnalytics from './components/GoogleAnalytics';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <BrowserRouter>
-      <GoogleAnalytics trackingId="UA-33461307-5">
-        <ParallaxProvider>
-          <Fragment>
-            <CssBaseline />
-            <App />
-          </Fragment>
-        </ParallaxProvider>
-      </GoogleAnalytics>
-    </BrowserRouter>
-  </MuiThemeProvider>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GoogleAnalytics trackingId="UA-33461307-5">
+          <ParallaxProvider>
+            <Fragment>
+              <CssBaseline />
+              <App />
+            </Fragment>
+          </ParallaxProvider>
+        </GoogleAnalytics>
+      </BrowserRouter>
+    </MuiThemeProvider>,
+    rootElement
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
