0221967675d32c3fb56b0a911a2b3534196f304b
Steve Fuller <wsfuller.dev@gmail.com>
2022-06-05 12:00:07 -0700

    Fix error with global styles
    - Missing semi-colon at the end of font-size

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -6,7 +6,7 @@ const GlobalStyles = createGlobalStyle`
   }
   body {
     font-family: ${({ theme }) => theme.tokens.typography.primary.family};
-    font-size: ${({ theme }) => `${theme.tokens.baseFontSize}px`}
+    font-size: ${({ theme }) => `${theme.tokens.baseFontSize}px`};
     color: ${({ theme }) => theme.color};
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
