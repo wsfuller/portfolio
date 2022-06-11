30f3c10ea30d1b1363c3ffa8f8c3661e571457b8
Steve Fuller <wsfuller.dev@gmail.com>
2022-06-11 15:21:14 -0700

    Update rendering and styles
    - Tweak action button styles
    - Update social media styles
    - Refactor social media rendering

--- src/components/AppBar/Menu/SocialMedia/SocialMedia.styles.ts
+++ src/components/AppBar/Menu/SocialMedia/SocialMedia.styles.ts
@@ -7,6 +7,10 @@ const StyledSocialMedia = styled.ul`
   margin-bottom: ${({ theme }) => theme.tokens.baseUnitsToRem(8)};
   padding: 0 ${({ theme }) => theme.tokens.baseUnitsToRem(4)};
   list-style: none;
+
+  & li a {
+    font-size: ${({ theme }) => theme.tokens.baseUnitsToRem(5)};
+  }
 `;
 
 export default StyledSocialMedia;
--- src/components/AppBar/Menu/SocialMedia/SocialMedia.tsx
+++ src/components/AppBar/Menu/SocialMedia/SocialMedia.tsx
@@ -4,32 +4,41 @@ import { FaMedium, FaStackOverflow, FaGithub, FaLinkedin } from 'react-icons/fa'
 import StyledSocialMedia from './SocialMedia.styles';
 import ActionButton from '../../ActionButton';
 
-const SocialMedia = () => (
-  <StyledSocialMedia>
-    <li>
-      <ActionButton as="a" anchorAttributes={{ href: 'https://github.com/wsfuller' }}>
-        <FaGithub />
-      </ActionButton>
-    </li>
-    <li>
-      <ActionButton as="a" anchorAttributes={{ href: 'https://www.linkedin.com/in/wsfuller-dev/' }}>
-        <FaLinkedin />
-      </ActionButton>
-    </li>
-    <li>
-      <ActionButton
-        as="a"
-        anchorAttributes={{ href: 'https://stackoverflow.com/users/3060325/wsfuller?tab=profile' }}
-      >
-        <FaStackOverflow />
-      </ActionButton>
-    </li>
-    <li>
-      <ActionButton as="a" anchorAttributes={{ href: 'https://medium.com/@wsfuller' }}>
-        <FaMedium />
-      </ActionButton>
-    </li>
-  </StyledSocialMedia>
-);
+const SocialMedia = () => {
+  const socialMediaItems = [
+    {
+      name: 'GitHub',
+      href: 'https://github.com/wsfuller',
+      icon: <FaGithub />,
+    },
+    {
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/wsfuller-dev/',
+      icon: <FaLinkedin />,
+    },
+    {
+      name: 'Stack Overflow',
+      href: 'https://stackoverflow.com/users/3060325/wsfuller?tab=profile',
+      icon: <FaStackOverflow />,
+    },
+    {
+      name: 'Medium',
+      href: 'https://medium.com/@wsfuller',
+      icon: <FaMedium />,
+    },
+  ];
+
+  return (
+    <StyledSocialMedia>
+      {socialMediaItems.map(({ name, href, icon }) => (
+        <li key={name}>
+          <ActionButton as="a" anchorAttributes={{ href }}>
+            {icon}
+          </ActionButton>
+        </li>
+      ))}
+    </StyledSocialMedia>
+  );
+};
 
 export default SocialMedia;
