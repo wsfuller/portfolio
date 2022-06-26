1af6f4cea81eabb9d13b602f3a3d7fd35ff8bc5b
Steve Fuller <wsfuller.dev@gmail.com>
2022-06-25 23:11:35 -0700

    Updating test suite
    - Adding footer test
    - Update deps for jest-dom
    - Add provider for jest-dom and extend expect
    - Remove unused .tsconfig

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { FaHeart } from 'react-icons/fa';
 import StyledFooter from './Footer.styles';
 
 const Footer: React.FC = () => (
-  <StyledFooter>
+  <StyledFooter data-testid="app-footer">
     Made with
     <FaHeart />
     <span>&copy;</span>
---
+++ src/test-utils.tsx
@@ -0,0 +1,18 @@
+import React, { FC, ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles, darkTheme } from './styles';
+
+const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => (
+  <ThemeProvider theme={darkTheme}>
+    <GlobalStyles />
+    {children}
+  </ThemeProvider>
+);
+
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
