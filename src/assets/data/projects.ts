3634a2432591ba34968c4ffbbb1264706f274f2d
Steve Fuller <wsfuller.dev@gmail.com>
2022-07-24 14:03:00 -0700

    Updating project data

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -1,20 +1,22 @@
-type ProjectItem = {
+export type ProjectItem = {
   id: string;
   name: string;
+  type: 'node' | 'react';
   description: string;
   demoUrl: string;
   githubUrl: string;
   tools: string[];
 };
 
-type Projects = ProjectItem[];
+export type Projects = ProjectItem[];
 
-const projects: Projects = [
+export const projects: Projects = [
   {
     id: 'movie-finder',
     name: 'Movie Finder',
+    type: 'react',
     description:
-      'Web application powered by The Movie Database API that allows users to search and save movies to a watch later list',
+      'Web application powered by The Movie Database API that allows users to search and save movies',
     demoUrl: 'https://wsf-moviefinder.netlify.app/',
     githubUrl: 'https://github.com/wsfuller/MovieFinder',
     tools: ['React', 'Redux', 'TypeScript', 'Fluent UI'],
@@ -22,6 +24,7 @@ const projects: Projects = [
   {
     id: 'react-weather',
     name: 'React Weather',
+    type: 'react',
     description:
       'Web application powered by the OpenWeatherMap API that allows for up to date daily weather reports',
     demoUrl: 'https://wsf-react-weather.netlify.app/',
@@ -31,6 +34,7 @@ const projects: Projects = [
   {
     id: 'elder-scrolls-legends',
     name: 'Elder Scrolls Legends',
+    type: 'react',
     description:
       'Web application powered by the Elder Scrolls Legends API that allows for searching and saving cards',
     demoUrl: 'https://elder-scrolls-legends.netlify.com/',
@@ -40,6 +44,7 @@ const projects: Projects = [
   {
     id: 'fetch-doggos',
     name: 'FetchDoggos',
+    type: 'react',
     description:
       'Web application powered by the DOG API that allows for fetching random dogs of specific breeds',
     demoUrl: 'https://fetch-doggos.netlify.app/',
@@ -49,6 +54,7 @@ const projects: Projects = [
   {
     id: 'wsfuller-resume-api',
     name: 'Resume API',
+    type: 'node',
     description: 'REST API for fetching my resume in JSON format',
     demoUrl: 'https://wsf-resume-api.herokuapp.com/',
     githubUrl: 'https://github.com/wsfuller/resume-api',
@@ -57,11 +63,10 @@ const projects: Projects = [
   {
     id: 'dark-remnants-podcast',
     name: 'Dark Remnants Podcast',
+    type: 'react',
     description: 'Web application showcasing episodes fo the Dark Remnants Podcast',
     demoUrl: 'http://dark-remnants-podcast.herokuapp.com/',
     githubUrl: 'https://github.com/wsfuller/dark-remnants-podcast',
     tools: ['React', 'Sass', 'Heroku'],
   },
 ];
-
-export default projects;
