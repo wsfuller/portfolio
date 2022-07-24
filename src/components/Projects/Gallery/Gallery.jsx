45fd338401ff59bb4dedb5947e761bb28df19514
Steve Fuller <wsfuller.dev@gmail.com>
2022-07-24 11:17:23 -0700

    Delete old project component work

--- src/components/Projects/Gallery/Gallery.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import GalleryList from './GalleryList';
-import projects from './galleryData';
-
-const Gallery = () => <GalleryList projects={projects} />;
-
-export default Gallery;
