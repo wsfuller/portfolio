6b36f5f8730010632894ba8b98975bed1dec6f11
Steve Fuller <wsfuller.dev@gmail.com>
2022-07-24 11:17:39 -0700

    Update project data

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -20,6 +20,15 @@ const projects: Projects = [
     tools: ['React', 'Redux', 'TypeScript', 'Fluent UI'],
   },
   {
+    id: 'react-weather',
+    name: 'React Weather',
+    description:
+      'Web application powered by the OpenWeatherMap API that allows for up to date daily weather reports',
+    demoUrl: 'https://wsf-react-weather.netlify.app/',
+    githubUrl: 'https://github.com/wsfuller/react-weather',
+    tools: ['React', 'Redux', 'TypeScript', 'Fluent UI'],
+  },
+  {
     id: 'elder-scrolls-legends',
     name: 'Elder Scrolls Legends',
     description:
@@ -27,6 +36,31 @@ const projects: Projects = [
     demoUrl: 'https://elder-scrolls-legends.netlify.com/',
     githubUrl: 'https://github.com/wsfuller/elder-scrolls-legends',
     tools: ['React', 'Redux', 'Material UI'],
+  },
+  {
+    id: 'fetch-doggos',
+    name: 'FetchDoggos',
+    description:
+      'Web application powered by the DOG API that allows for fetching random dogs of specific breeds',
+    demoUrl: 'https://fetch-doggos.netlify.app/',
+    githubUrl: 'https://github.com/wsfuller/fetchDoggos',
+    tools: ['React', 'Redux', 'Styled Components', 'Semantic UI'],
+  },
+  {
+    id: 'wsfuller-resume-api',
+    name: 'Resume API',
+    description: 'REST API for fetching my resume in JSON format',
+    demoUrl: 'https://wsf-resume-api.herokuapp.com/',
+    githubUrl: 'https://github.com/wsfuller/resume-api',
+    tools: ['Node', 'Express', 'Mongo DB', 'Heroku'],
+  },
+  {
+    id: 'dark-remnants-podcast',
+    name: 'Dark Remnants Podcast',
+    description: 'Web application showcasing episodes fo the Dark Remnants Podcast',
+    demoUrl: 'http://dark-remnants-podcast.herokuapp.com/',
+    githubUrl: 'https://github.com/wsfuller/dark-remnants-podcast',
+    tools: ['React', 'Sass', 'Heroku'],
   },
 ];
 
